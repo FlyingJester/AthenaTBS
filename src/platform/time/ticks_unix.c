58bfb62375e817f9f9e4efda72a490fe34861160
FlyingJester <foolkingcrown@gmail.com>
2015-10-10 15:11:58 -0800

    Added monotonic timer to platform

---
+++ src/platform/time/ticks_unix.c
@@ -0,0 +1,12 @@
+#include "ticks.h"
+#include <sys/time.h>
+
+unsigned long Athena_GetMillisecondTicks(){
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    {
+        unsigned long time_ms = t.tv_sec * 1000;
+        time_ms += t.tv_usec / 1000;
+        return time_ms;
+    }
+}
