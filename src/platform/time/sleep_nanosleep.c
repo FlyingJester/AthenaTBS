eb6783356b4ed306a0041c8fcb0014e3078030d8
FlyingJester <foolkingcrown@gmail.com>
2015-10-10 16:19:53 -0800

    Added Linux sleep backend

---
+++ src/platform/time/sleep_nanosleep.c
@@ -0,0 +1,10 @@
+#include <time.h>
+
+void Athena_MillisecondSleep(unsigned ms){
+    struct timespec t;
+    unsigned long ns = ms * 1000000;
+    t.tv_sec = ms / 1000;
+    ns -= (t.tv_sec * 1000000000);
+    
+    nanoslep(&t, NULL);
+}
