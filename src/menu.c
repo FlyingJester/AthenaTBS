f14979ed32dfe6a078a03473eb49a837eb82f8cb
FlyingJester <foolkingcrown@gmail.com>
2015-10-18 03:22:38 -0800

    Added menu files

---
+++ src/menu.c
@@ -0,0 +1,18 @@
+#include "menu.h"
+
+static int athena_set_menu_position(struct Athena_ButtonList *buttons, int x, int last_y, int w, int h){
+    if(!buttons){
+        return 0;
+    }
+    else{
+        buttons->button.x = x;
+        buttons->button.y = last_y;
+        buttons->button.w = w;
+        buttons->button.h = h;
+        return athena_set_menu_position(buttons->next, x, last_y + h, w, h);
+    }
+}
+
+void Athena_OrganizeMenu(struct Athena_Menu *menu){
+    athena_set_menu_position(menu->buttons, menu->x + 4, menu->y + 4, menu->w - 8, 20);
+}
---
+++ src/menu.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "button.h"
+
+void Athena_OrganizeMenu(struct Athena_Menu *menu);
+
