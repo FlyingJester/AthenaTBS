35f9b87d528b2d5b3cc494ced61ab80174944636
FlyingJester <foolkingcrown@gmail.com>
2015-10-19 14:22:06 -0800

    Added windows thread support

---
+++ src/platform/thread/thread_win32.c
@@ -0,0 +1,35 @@
+#include "thread.h"
+#include <windows.h>
+
+struct Athena_Thread{
+	void *arg;
+	void (*func)(void *);
+	HANDLE thread;
+}
+
+static DWORD WINAPI athena_win32_thread_wrapper(LPVOID arg){
+	struct Athena_Thread * const thr = arg;
+	thr->func(thr->arg);
+	return 0;
+}
+
+struct Athena_Thread *Athena_CreateThread(void (*thread_function)(void *), void *arg){
+	struct Athena_Thread * const thr = malloc(sizeof(struct Athena_Thread));
+	thr->arg = arg;
+	thr->func = thread_function;
+	thr->thread = NULL;
+	return thr;
+}
+
+void Athena_StartThread(struct Athena_Thread *thr){
+	thr->thread = CreateThread(NULL, 0, athena_win32_thread_wrapper, thr, 0);
+}
+
+void Athena_JoinThread(struct Athena_Thread *thr){
+	WaitForSingleObject(thr->thread, INFINITE);
+}
+
+void Athena_DestroyThread(struct Athena_Thread *thr){
+	free(thr);
+}
+
